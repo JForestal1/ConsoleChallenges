ce0e9a2faf4b00b21416f5f8f769ac7cd1dac473
JForestal1 <jforestal1@gmail.com>
2020-12-13 22:08:32 -0500

    first commit

---
+++ ConsoleChallenge2/ProgramUI.cs
@@ -0,0 +1,75 @@
+﻿using ConsoleChallenge2_Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleChallenge2
+{
+    class ProgramUI
+    {
+        // Instantiate the claim repo
+        public ClaimRepo queueOfClaims = new ClaimRepo();
+
+        public void Run()
+        {
+            Menu();
+        }
+
+        // Menu
+
+        public void Menu()
+        {
+            bool active = true;
+            while (active)
+            {
+                Console.WriteLine("Select an Option:\n" +
+                "1. See All Claims\n" +
+                "2. Take care of next claim\n" +
+                "3. Enter a new claim\n" +
+                "X - Exit");
+                string choice = Console.ReadLine().ToLower();
+                switch (choice)
+                {
+                    case "1":
+                        {
+                            Console.Clear();
+                            //View All Claims
+                            break;
+                        }
+                    case "2":
+                        {
+                            Console.Clear();
+                            //Process next claim 
+                            break;
+                        }
+                    case "3":
+                        {
+                            Console.Clear();
+                            //Enter a new claim
+                            break;
+                        }
+                    case "x":
+                        {
+                            Console.WriteLine("Thanks for coming by");
+                            Console.Beep(1000, 500);
+                            active = false;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Please enter a valid choice!");
+                            break;
+                        }
+                }
+                if (active)
+                {
+                    Console.WriteLine("\nPress any key to continue");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleChallenge2_repo/ClaimRepo.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleChallenge2_Repo.Claim;
+
+namespace ConsoleChallenge2_Repo
+{
+    // Create an instance of the claim queue
+    public class ClaimRepo
+    {
+        Queue<Claim> ClaimQueue = new Queue<Claim>();
+
+        // Add a claim to the queue with a complete claim
+        public bool AddToQueue(Claim claimToAdd)
+        {
+            if (ClaimIdIsUnique(claimToAdd.ClaimID))
+            {
+                ClaimQueue.Enqueue(claimToAdd);
+                return true;
+            }
+            return false;
+
+        }
+
+        // Add a claim to the queue with the elements of a claim
+        public bool AddToQueue(int ID, ClaimType type, string description, double amount, DateTime dateOfIncident, DateTime dateOfClaim, bool isValid)
+        {
+            if (ClaimIdIsUnique(ID))
+            {
+                Claim claimToAdd = new Claim();
+                claimToAdd.ClaimID = ID;
+                claimToAdd.Type = type;
+                claimToAdd.Description = description;
+                claimToAdd.ClaimAmount = amount;
+                claimToAdd.DateOfIncident = dateOfIncident;
+                claimToAdd.DateOfClaim = dateOfClaim;
+                claimToAdd.IsValid = isValid;
+                ClaimQueue.Enqueue(claimToAdd);
+                return true;
+            }
+            return false;
+
+        }
+        public bool ClaimIdIsUnique(int ID)
+        {
+            bool idIsUnique = true;
+            foreach (Claim claimToTest in ClaimQueue)
+            {
+                if (ID == claimToTest.ClaimID)
+                {
+                    idIsUnique = false;
+                }
+            }
+            return idIsUnique;
+        }
+    }
+}
+
+
+//    // Get menu item
+//    public MenuItems GetMenuItemByName(string nameToGet)
+//    {
+//        foreach (var itemToSearchFor in _listOfMenuItems)
+//        {
+//            if (itemToSearchFor.MenuName.ToLower() == nameToGet.ToLower())
+//                return itemToSearchFor;
+//        }
+
+//        return null;
+//    }
+//    // add menu item
+//    public bool AddMenuItem(MenuItems newItem)
+//    {
+//        int itemCountBeforeAdd = _listOfMenuItems.Count;
+//        _listOfMenuItems.Add(newItem);
+//        if (_listOfMenuItems.Count != itemCountBeforeAdd + 1)
+//            return false;
+//        else
+//            return true;
+//    }
+//    // delete menu item
+//    public bool RemoveMenuItem(string itemNameToDelelete)
+//    {
+//        {
+//            MenuItems itemToDelete = GetMenuItemByName(itemNameToDelelete);
+//            if (itemToDelete != null)
+//            {
+//                _listOfMenuItems.Remove(itemToDelete);
+//                return true;
+//            }
+//            else
+//                return false;
+//        }
+//    }
+//    public bool UpdateMenuItem(string itemNameToUpdate, MenuItems itemToUpdate)
+//    {
+//        bool recordFound = false;
+
+//        foreach (MenuItems allItems in _listOfMenuItems)
+//        {
+//            if (allItems.MenuName.ToLower() == itemNameToUpdate.ToLower())
+//            {
+//                recordFound = true;
+//                allItems.MenuItem = itemToUpdate.MenuItem;
+//                allItems.Description = itemToUpdate.Description;
+//                allItems.MenuName = itemToUpdate.MenuName;
+//                allItems.Price = itemToUpdate.Price;
+//            }
+//        }
+//        return recordFound;
+//    }
+//    // Update menu item ingredients
+//    public bool UpdateMenuItemIngredients(string itemNameToUpdate, string ingredientToEdit, Ingredients updatedIngredient)
+//    {
+//        bool recordFound = false;
+
+//        foreach (MenuItems allItems in _listOfMenuItems)
+//        {
+//            if (allItems.MenuName.ToLower() == itemNameToUpdate.ToLower())
+//            {
+//                foreach (Ingredients allIngredients in allItems._ListOfIngredients)
+//                {
+//                    if (allIngredients.Item == ingredientToEdit)
+//                    {
+//                        recordFound = true;
+//                        allIngredients.Item = updatedIngredient.Item;
+//                        allIngredients.Quantity = updatedIngredient.Quantity;
+//                        allIngredients.Units = updatedIngredient.Units;
+//                    }
+//                }
+//            }
+//        }
+//        return recordFound;
+//    }
+//    // show all menu items
+//    public List<MenuItems> GetEntireMenu()
+//    {
+//        return _listOfMenuItems;
+//    }
+//}
+
