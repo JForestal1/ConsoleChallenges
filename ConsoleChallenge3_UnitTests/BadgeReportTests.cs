bdde6ef80428bd112c0f8484837b37d201670422
JForestal1 <jforestal1@gmail.com>
2020-12-15 19:36:21 -0500

    Ad and get single methods tested for App 3

---
+++ ConsoleChallenge3_UnitTests/BadgeReportTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ConsoleChallenge3_repo;
+using System.Collections.Generic;
+
+namespace ConsoleChallenge3_UnitTests
+{
+    [TestClass]
+    public class BadgeReportTests
+    {
+        BadgeRepo testBadgeRepo = new ConsoleChallenge3_repo.BadgeRepo();
+
+        [TestMethod]
+        public void TestAddUniqueKey()
+        {
+            // arrange
+            List<string> testDoors = new List<string>();
+            testDoors.Add("A1");
+            testDoors.Add("D3");
+            testDoors.Add("F4");
+
+            // act and assert
+            Assert.IsTrue(testBadgeRepo.AddBadge(1, testDoors));
+        }
+        [TestMethod]
+        public void TestAddNOTUniqueKey()
+        {
+            // arrange
+            List<string> testDoors = new List<string>();
+            testDoors.Add("A1");
+            testDoors.Add("D3");
+            testDoors.Add("F4");
+            testBadgeRepo.AddBadge(1, testDoors);
+
+            // act and assert
+            Assert.IsFalse(testBadgeRepo.AddBadge(1, testDoors));
+        }
+
+        [TestMethod]
+        public void testGetSingleBadge()
+        {
+            // arrange
+            List<string> testDoors = new List<string>();
+            testDoors.Add("A1");
+            testDoors.Add("D3");
+            testDoors.Add("F4");
+            testBadgeRepo.AddBadge(1, testDoors);
+
+            // act and assert
+            Assert.IsNotNull(testBadgeRepo.GetSingleBadge(1));
+        }
+    }
+}
---
+++ ConsoleChallenge3_repo/BadgeRepo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleChallenge3_repo
+{
+    public class BadgeRepo
+    {
+        Dictionary<int, List<string>> Badges = new Dictionary<int, List<string>>();
+
+        public bool AddBadge(int newBadgeNumber, List<string> _doors)
+        {
+            if (BadgeNumberIsUnique(newBadgeNumber))
+            {
+                Badges.Add(newBadgeNumber, _doors);
+                return true;
+            }
+            else
+                return false;
+
+        }
+
+        public bool BadgeNumberIsUnique(int badgeToTest)
+        {
+            foreach (KeyValuePair<int, List<string>> badge in Badges)
+            {
+                if (badgeToTest == badge.Key)
+                    return false;
+            }
+            return true;
+        }
+
+        public Badge GetSingleBadge(int badgeIDToGet)
+        {
+            Badge returnBadge = new Badge();
+            foreach (KeyValuePair<int, List<string>> badge in Badges)
+            {
+                if (badgeIDToGet == badge.Key)
+                {
+                    returnBadge.BadgeID = badge.Key;
+                    returnBadge.Doors = badge.Value;
+                }
+                return returnBadge;
+            }
+            return null;
+        }
+    }
+}
+
