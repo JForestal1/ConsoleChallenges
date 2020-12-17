e0f865f1cb9507dccd36b6b3f3bfdb1c786aaaa2
JForestal1 <jforestal1@gmail.com>
2020-12-17 15:09:13 -0500

    Added App 4 tests

---
+++ ConsoleChallenge5_repo/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleChallenge5_repo
+{
+    public class Customer
+    {
+        public enum CustomerType
+        {
+            Potential = 1,
+            Current,
+            Past
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public CustomerType Type { get; set; }
+        public string EmailGreeting { get; set; }
+
+        public Customer() { }
+
+        public Customer(string firstname, string lastname, CustomerType type, string email)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Type = type;
+            EmailGreeting = email;
+        }
+    }
+}
---
+++ ConsoleChallenge5_repo/CustomerRepo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleChallenge5_repo
+{
+    class CustomerRepo
+    {
+        SortedList<string, Customer> _listOfCustomers = new SortedList<string, Customer>();
+
+        public bool AddCustomer(string first, string last, Customer.CustomerType type)
+        {
+            Customer customerToAdd = new Customer();
+            customerToAdd.FirstName = first;
+            customerToAdd.LastName = last;
+            customerToAdd.Type = type;
+            switch (type)
+            {
+                case Customer.CustomerType.Potential:
+                    {
+                        customerToAdd.EmailGreeting = "We currently have the lowest rates on Helicopter Insurance!";
+                        break;
+                    }
+                case Customer.CustomerType.Current:
+                    {
+                        customerToAdd.EmailGreeting = "Thank you for your work with us. We appreciate your loyalty. Here's a coupon.";
+                        break;
+                    }
+                case Customer.CustomerType.Past:
+                    {
+                        customerToAdd.EmailGreeting = "It's been a long time since we've heard from you, we want you back";
+                        break;
+                    }
+            }
+            int listCountBefore = _listOfCustomers.Count;
+            _listOfCustomers.Add(last + first, customerToAdd);
+            if (listCountBefore < _listOfCustomers.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
